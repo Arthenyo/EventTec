1529fe460c6abd4270ec1673515959468370dfcd
Arthenyo <88592884+Arthenyo@users.noreply.github.com>
2024-08-06 15:06:36 -0300

    feat: add repositories

---
+++ api/src/main/java/com/enventostec/api/repositories/CouponRepository.java
@@ -0,0 +1,9 @@
+package com.enventostec.api.repositories;
+
+import com.enventostec.api.domain.coupon.Coupon;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.UUID;
+
+public interface CouponRepository extends JpaRepository<Coupon, UUID> {
+}
---
+++ api/src/main/java/com/enventostec/api/repositories/EventRepository.java
@@ -0,0 +1,9 @@
+package com.enventostec.api.repositories;
+
+import com.enventostec.api.domain.event.Event;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.UUID;
+
+public interface EventRepository extends JpaRepository<Event, UUID> {
+}
